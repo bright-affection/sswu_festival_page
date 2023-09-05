75e8f44a5b7c10d87ed9741ebc1110832dd7ae5f
maeng <20221153@sungshin.ac.kr>
2023-09-05 17:12:12 +0900

    feat:굿즈예약 기능

---
+++ BACKEND/HYUNSOO/sswu/order/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Goods(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='order', null=True)
+
+DATE_CHOICES = [
+    ('2023-10-04', '10월 4일'),
+    ('2023-10-05', '10월 5일'),
+]
+
+TIME_CHOICES = [(f"{i}:00", f"{i}:00") for i in range(11, 20)]
+
+class Reservation(models.Model):
+    user_name = models.CharField(max_length=100)
+    phone_number = models.IntegerField()
+    user_mail = models.EmailField()
+   # goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    purchase_date = models.CharField(max_length=10, choices=DATE_CHOICES)
+    purchase_time = models.CharField(max_length=5, choices=TIME_CHOICES)
+    
+class OrderItem(models.Model):
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, related_name='item')
+    goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
---
+++ BACKEND/HYUNSOO/sswu/order/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+app_name = 'order'
+
+urlpatterns = [
+    path('goods_list/', views.goods_list, name='goods_list'),
+    path('reserve/', views.reserve_goods, name='reserve'),
+]
