89da1db7c17b9ed9be8ac6527100472797419d00
sj <hera4224@naver.com>
2023-09-09 18:54:20 +0900

    feat:부스 필터

--- BACKEND/SUJIN/config/urls.py
+++ BACKEND/SUJIN/config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +25,4 @@ urlpatterns = [
     path('booth/', include('booth.urls', namespace='booth')),
     path('visitor/', include('visitor.urls', namespace='visitor')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root =  settings.MEDIA_ROOT)
