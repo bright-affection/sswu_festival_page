77baec8546242a55a90c610f400bdaec70845c72
LGAIN <heli1420@naver.com>
2023-09-10 02:50:06 +0900

    fix:info 이미지 업로드

--- BACKEND/LEEGAIN/mainpage/admin.py
+++ BACKEND/LEEGAIN/mainpage/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Info
+from .models import Info, InfoImage
 
-class Info(admin.ModelAdmin):
-    list_display = ['id', 'title', 'content', 'image', 'created_at', 'updated_at']
+class InfoImageInline(admin.TabularInline):
+    model = InfoImage
+    extra = 1
+
+class InfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content']
     list_display_links = ['id', 'title']
     search_fields = ['title', 'content']
+    inlines = [InfoImageInline]
+
+admin.site.register(Info, InfoAdmin)
+admin.site.register(InfoImage)
