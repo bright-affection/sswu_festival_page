f4438abce587a380b8bad11bfb4ea6a25b5c51c5
yenqkr <sksksk951@naver.com>
2023-09-13 17:47:07 +0900

    [fix] 예약페이지 연결

--- BACKEND/HYUNSOO/sswu/order/models.py
+++ BACKEND/HYUNSOO/sswu/order/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Goods(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField()
@@ -7,13 +8,13 @@ class Goods(models.Model):
 
     def __str__(self):
         return self.name
-    def __str__(self):
-        return self.price
+
 
 DATE_CHOICES = [
     ('2023-10-04', '10월 4일'),
     ('2023-10-05', '10월 5일'),
 ]
+
 
 TIME_CHOICES = [(f"{i}:00", f"{i}:00") for i in range(11, 20)]
 
@@ -21,12 +22,15 @@ class Reservation(models.Model):
     user_name = models.CharField(max_length=100)
     phone_number = models.IntegerField()
     user_mail = models.EmailField()
-   # goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
-   # quantity = models.PositiveIntegerField()
     purchase_date = models.CharField(max_length=10, choices=DATE_CHOICES)
     purchase_time = models.CharField(max_length=5, choices=TIME_CHOICES)
+    reserved_at = models.DateTimeField(auto_now_add=True)
+
     
 class OrderItem(models.Model):
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, related_name='item')
     goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.goods.name
--- BACKEND/HYUNSOO/sswu/sswu/settings.py
+++ BACKEND/HYUNSOO/sswu/sswu/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'order',
+    'rangefilter',
 ]
 
 MIDDLEWARE = [
@@ -94,9 +95,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -106,7 +107,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR/ 'static',
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
