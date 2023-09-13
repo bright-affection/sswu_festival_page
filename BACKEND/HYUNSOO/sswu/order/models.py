1b10a153c0a0baed2f0f3106c3f45befb11abaf9
maeng <20221153@sungshin.ac.kr>
2023-09-13 16:13:00 +0900

    fix:models수정

--- BACKEND/HYUNSOO/sswu/order/models.py
+++ BACKEND/HYUNSOO/sswu/order/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Goods(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField()
@@ -8,23 +9,25 @@ class Goods(models.Model):
     def __str__(self):
         return self.name
 
+
 DATE_CHOICES = [
     ('2023-10-04', '10월 4일'),
     ('2023-10-05', '10월 5일'),
 ]
 
+
 TIME_CHOICES = [(f"{i}:00", f"{i}:00") for i in range(11, 20)]
+
 
 class Reservation(models.Model):
     user_name = models.CharField(max_length=100)
     phone_number = models.IntegerField()
     user_mail = models.EmailField()
-   # goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
-   # quantity = models.PositiveIntegerField()
     purchase_date = models.CharField(max_length=10, choices=DATE_CHOICES)
     purchase_time = models.CharField(max_length=5, choices=TIME_CHOICES)
     reserved_at = models.DateTimeField(auto_now_add=True)
-    
+
+
 class OrderItem(models.Model):
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, related_name='item')
     goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
@@ -32,3 +35,4 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return self.goods.name
+
