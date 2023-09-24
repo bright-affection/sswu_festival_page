357e69a53c94baedd51a202db256f7c3ac2fee60
sj <hera4224@naver.com>
2023-09-25 00:58:01 +0900

    feat:프론트 2차 연결 완료

--- BACKEND/SUJIN/booth/views.py
+++ BACKEND/SUJIN/booth/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 from .models import Booth
-from django.db.models import Q
+from django.db.models import Q,F,Case, When, Value, CharField, IntegerField,ExpressionWrapper
+from django.db.models.functions import Cast
+from django.db.models.functions import Substr
 
 def boothinfo(request):
     if request.method == 'GET':
-        booth_list = Booth.objects.all().order_by('name')
+        booth_list = Booth.objects.all().annotate(
+    prefix=ExpressionWrapper(
+        Substr(F('locate'), 1, 1),  # 'locate' 필드의 첫 번째 문자 추출
+        output_field=CharField()
+    ),
+    numeric_locate=ExpressionWrapper(
+        Case(
+            When(prefix='A', then=Value(1)),  
+            When(prefix='B', then=Value(2)),  
+            When(prefix='C', then=Value(3)),  
+            When(prefix='D', then=Value(4)),  
+            When(prefix='E', then=Value(5)),  
+            When(prefix='F', then=Value(6)),  
+            default=Value(0),  # 다른 경우 0으로 설정하거나 다른 기본값 사용
+            output_field=IntegerField()
+        ) * 1000 +  # 여기서 1000을 곱해 "a", "b", "e"를 구분하고, 각 그룹 내에서 숫자를 정렬합니다.
+        Cast(Substr(F('locate'), 3), IntegerField()),  # 문자열의 세 번째 문자부터 추출하여 정수로 변환
+        output_field=IntegerField()
+    )
+).order_by('numeric_locate')
         context = {
             'booth_list' : booth_list
         }
@@ -13,7 +34,26 @@ def boothinfo(request):
         #검색어
         if 'search' in request.POST:
             searched = request.POST['search']    
-            booth_list = Booth.objects.filter(name__icontains=searched).order_by('name')
+            booth_list = Booth.objects.filter(name__icontains=searched).annotate(
+            prefix=ExpressionWrapper(
+                Substr(F('locate'), 1, 1),  # 'locate' 필드의 첫 번째 문자 추출
+                output_field=CharField()
+            ),
+            numeric_locate=ExpressionWrapper(
+                Case(
+                    When(prefix='A', then=Value(1)),  
+                    When(prefix='B', then=Value(2)),  
+                    When(prefix='C', then=Value(3)),  
+                    When(prefix='D', then=Value(4)),  
+                    When(prefix='E', then=Value(5)),  
+                    When(prefix='F', then=Value(6)),  
+                    default=Value(0),  # 다른 경우 0으로 설정하거나 다른 기본값 사용
+                    output_field=IntegerField()
+                ) * 1000 +  # 여기서 1000을 곱해 "a", "b", "e"를 구분하고, 각 그룹 내에서 숫자를 정렬합니다.
+                Cast(Substr(F('locate'), 3), IntegerField()),  # 문자열의 세 번째 문자부터 추출하여 정수로 변환
+                output_field=IntegerField()
+            )
+        ).order_by('numeric_locate')
             context = {
                 'booth_list' : booth_list
             }
@@ -26,13 +66,37 @@ def boothinfo(request):
 
             q=Q()
             if date:
-                q &= Q(date__in = date)
+                # q &= Q(date__icontains=date)
+                for data in date:
+                    cleaned_date = data.replace(" ", "").lower()  # 데이터 정제 및 비교
+                    q &= Q(date__icontains=cleaned_date)
             if time:
-                q &= Q(time__in = time)
+                for dataa in time:
+                    cleaned_datee = dataa.replace(" ", "").lower()  # 데이터 정제 및 비교
+                    q &= Q(time__icontains=cleaned_datee)
             if kind:
                 q &= Q(kind__in = kind)
                         
-            booth_list = Booth.objects.filter(q).order_by('name')
+            booth_list = Booth.objects.filter(q).annotate(
+            prefix=ExpressionWrapper(
+                Substr(F('locate'), 1, 1),  # 'locate' 필드의 첫 번째 문자 추출
+                output_field=CharField()
+            ),
+            numeric_locate=ExpressionWrapper(
+                Case(
+                    When(prefix='A', then=Value(1)),  
+                    When(prefix='B', then=Value(2)),  
+                    When(prefix='C', then=Value(3)),  
+                    When(prefix='D', then=Value(4)),  
+                    When(prefix='E', then=Value(5)),  
+                    When(prefix='F', then=Value(6)),  
+                    default=Value(0),  # 다른 경우 0으로 설정하거나 다른 기본값 사용
+                    output_field=IntegerField()
+                ) * 1000 +  # 여기서 1000을 곱해 "a", "b", "e"를 구분하고, 각 그룹 내에서 숫자를 정렬합니다.
+                Cast(Substr(F('locate'), 3), IntegerField()),  # 문자열의 세 번째 문자부터 추출하여 정수로 변환
+                output_field=IntegerField()
+            )
+        ).order_by('numeric_locate')
             context = {
                 'booth_list' : booth_list
             }
