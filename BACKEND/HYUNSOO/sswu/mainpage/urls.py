bb04e35b4e655e17b55588057c68e27123d064a7
Lee <i876838@gmail.com>
2023-09-26 20:18:00 +0900

    mainpage url 변경

--- BACKEND/HYUNSOO/sswu/mainpage/urls.py
+++ BACKEND/HYUNSOO/sswu/mainpage/urls.py
@@ -5,10 +5,8 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = 'mainpage'
-
 urlpatterns = [
-    path('mainpage/', views.mainpage, name='mainpage'),
+    path('', views.mainpage, name='mainpage'),
     path('administrator/', views.administrator, name='administrator'),
     path('info/', views.info_list, name='info'),
     path('info_write/', views.info_write, name='info_write'),
