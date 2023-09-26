d8da04817ffedffd632002d8e34a6b1411adc538
bright-affection <golliaz@naver.com>
2023-09-26 21:19:07 +0900

    visitor 수정

--- BACKEND/HYUNSOO/sswu/visitor/urls.py
+++ BACKEND/HYUNSOO/sswu/visitor/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import visitor,visitor_write,visitor_more
+from visitor.views import visitor_json
 
 app_name = 'visitor'
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('', visitor, name='visitor'),
     path('write/', visitor_write, name='visitor_write'),
     path('more/', visitor_more, name='visitor_more'),
+    path('visitor.json', visitor_json, name='visitor_json')
 ]
