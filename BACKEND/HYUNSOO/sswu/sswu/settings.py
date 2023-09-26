c7eedbf924e02b5d48bfcdc6f538c7f073be2330
maeng <89590797+maeng-kim@users.noreply.github.com>
2023-09-27 02:27:50 +0900

    Update settings.py

--- BACKEND/HYUNSOO/sswu/sswu/settings.py
+++ BACKEND/HYUNSOO/sswu/sswu/settings.py
@@ -123,11 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    BASE_DIR/ 'static',
-] 
+    os.path.join(BASE_DIR, 'static_files'),
+    ] 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
