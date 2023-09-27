f1e03806c3d93bc2ee893f753badff8e72a4c635
maeng <20221153@sungshin.ac.kr>
2023-09-27 09:22:09 +0900

    fix:makers 수정

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('info/', views.info_list, name='info'),
     path('info_write/', views.info_write, name='info_write'),
     path('info_post/<int:info_id>/', views.info_post, name='info_post'),
+    path('makers/', views.makers, name='makers'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
